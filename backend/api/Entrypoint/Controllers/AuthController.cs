d59c36fa9ea2f6a10a3adadba7e17b87f3c577ee
Karl Marx Roxas <karlm.roxas@gmail.com>
2026-06-18 09:59:54 +0800

    fix: samesite strict

--- backend/api/Entrypoint/Controllers/AuthController.cs
+++ backend/api/Entrypoint/Controllers/AuthController.cs
@@ -76,7 +76,7 @@ public class AuthController : ControllerBase
         {
             HttpOnly = true,
             Secure = true,
-            SameSite = SameSiteMode.Strict,
+            SameSite = SameSiteMode.None,
             Expires = DateTime.UtcNow.AddDays(7)
         };
 
